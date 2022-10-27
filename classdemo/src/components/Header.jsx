738fc982ea3a2d44a7078dd893cd6a85adce12eb
austinProGit <lee_austin@columbusstate.edu>
2022-10-27 16:30:13 +0000

    stateful components

--- classdemo/src/components/Header.jsx
+++ classdemo/src/components/Header.jsx
@@ -2,15 +2,26 @@ import React from "react";
 
 class Header extends React.Component{
     // constructor for state
-
+    constructor(props){
+        super(props)
+        this.state = { 
+            count: 0, 
+            theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+            // theme: 'dark'
+        }
+    }
+    
     //lifecycle methods
 
     // render method
     render(){
         const { linkText, linkUrl } = this.props
+        // this.props.linkText
+        console.log(this.state)
+        console.log(window.matchMedia('(prefers-color-scheme: dark)').matches)
         return(
-            <header className="App-header">
-                <p>
+            <header className={`App-header ${this.state.theme} ${this.state.count}`}>
+                <p className={"testing-this " + this.state.theme}>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
@@ -20,6 +31,12 @@ class Header extends React.Component{
                     rel="noopener noreferrer">
                     {linkText}
                 </a>
+                <span>
+                    You have clicked on the button {this.state.count} times!
+                </span>
+                <button onClick={() => this.setState({ count : this.state.count + 1})}>
+                    Increment Count
+                </button>
             </header>            
         )
     }
